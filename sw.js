8eeb419b3827371160cad4e9f01b0c652f0673e0
christopher-brock <47276544+christopher-brock@users.noreply.github.com>
2026-09-07 23:53:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this string whenever you upload new files.
-const CACHE = 'essentials-v2';
+const CACHE = 'essentials-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
